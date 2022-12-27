023243444b89e94d25d693765ae133e381158a78
Tootin0730 <yisan1004@gmail.com>
2022-12-27 17:32:59 -0500

    #2.0 Painting Lines (fixed.ver)
    The mouse and the line might not match depending on the user's computer model.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 800;
+canvas.width = 500;
+canvas.height = 500;
 ctx.lineWidth = 2;
 
 const colors = ["#ff3838", "ffb8b8", "c56cf0", "#ff9f1a", "fff200", "#32ff7e"];
